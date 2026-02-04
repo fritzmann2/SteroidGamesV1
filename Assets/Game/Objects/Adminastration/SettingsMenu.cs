812da4e06e9609dd38d312c075a503c3829e89d4
fritzmann2 <gagabais994@gmail.com>
2026-02-04 16:42:32 +0100

    refactor: Replace movement script with PlayerMovement and clean up SettingsMenu

--- Assets/Game/Objects/Adminastration/SettingsMenu.cs
+++ Assets/Game/Objects/Adminastration/SettingsMenu.cs
@@ -5,13 +5,10 @@ using Unity.Netcode;
 
 public class SettingsMenu : MonoBehaviour
 {
-    public TMP_InputField inputDashforce;
-    public TMP_InputField inputMoveSpeed;
-    public TMP_InputField Jumpforce;
     public TMP_InputField GravityScale;
     
 
-    private movement localMovementScript; 
+    private PlayerMovement localMovementScript; 
 
     void OnEnable()
     {
@@ -27,15 +24,9 @@ public class SettingsMenu : MonoBehaviour
         }
 
         GameObject myPlayer = NetworkManager.Singleton.LocalClient.PlayerObject.gameObject;
-        localMovementScript = myPlayer.GetComponent<movement>();
+        localMovementScript = myPlayer.GetComponent<PlayerMovement>();
         if (localMovementScript != null)
-        {
-            if(inputDashforce != null) inputDashforce.text = localMovementScript.dashforce.ToString();
-            
-            if(inputMoveSpeed != null) inputMoveSpeed.text = localMovementScript.basemoveSpeed.ToString(); 
-            
-            if(Jumpforce != null) Jumpforce.text = localMovementScript.jumpForce.ToString();
-            
+        {            
             Rigidbody2D rb = myPlayer.GetComponent<Rigidbody2D>();
             if (rb != null && GravityScale != null)
             {
@@ -49,21 +40,6 @@ public class SettingsMenu : MonoBehaviour
         if (localMovementScript == null) 
         {
             if (localMovementScript == null) return;
-        }
-
-        if (inputDashforce != null && float.TryParse(inputDashforce.text, out float dashVal))
-        {
-             localMovementScript.dashforce = dashVal;
-        }
-
-        if (inputMoveSpeed != null && float.TryParse(inputMoveSpeed.text, out float speedVal))
-        {
-            localMovementScript.basemoveSpeed = (int)speedVal; 
-        }
-
-        if (Jumpforce != null && float.TryParse(Jumpforce.text, out float jumpVal))
-        {
-            localMovementScript.jumpForce = jumpVal;
         }
 
         if (GravityScale != null && float.TryParse(GravityScale.text, out float gravVal))
